e8b4b52ecb6d9af690af2583d31e42d94be07fbd
Marek Szumny <lukmarcus@gmail.com>
2025-01-31 17:08:10 +0100

    parent elements added

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -22,3 +22,12 @@ test('Locating child elements', async ({ page }) => {
   await page.locator('nb-card').getByRole('button', {name: 'Sign in'}).first().click()
   await page.locator('nb-card').nth(3).getByRole('button').click()
 });
+
+test('Locating parent elements', async ({ page }) => {
+  await page.locator('nb-card', {hasText: "Using the Grid"}).getByRole('textbox', {name: "Email"}).click()
+  await page.locator('nb-card', {has: page.locator('#inputEmail1')}).getByRole('textbox', {name: "Email"}).click()
+  await page.locator('nb-card').filter({hasText: 'Basic form'}).getByRole('textbox', {name: "Email"}).click()
+  await page.locator('nb-card').filter({has: page.locator('.status-danger')}).getByRole('textbox', {name: "Password"}).click()
+  await page.locator('nb-card').filter({has: page.locator('nb-checkbox')}).filter({hasText: 'Sign in'}).getByRole('textbox', {name: "Email"}).click()
+  await page.locator(':text-is("Using the grid")').locator('..').getByRole('textbox', {name: "Email"}).click()
+})
