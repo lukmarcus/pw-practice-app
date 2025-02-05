af24d711bdc742fed6f4150536e0279f4d96839d
Marek Szumny <lukmarcus@gmail.com>
2025-02-05 21:33:01 +0100

    radio buttons test added

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -39,5 +39,32 @@ test.describe("Form Layouts page", () => {
       }
     );
   });
-});
 
+  test("Radio buttons", async ({ page }) => {
+    const usingTheGridForm = page.locator("nb-card", {
+      hasText: "Using the grid",
+    });
+
+    await usingTheGridForm.getByLabel("Option 1").check({ force: true });
+    await usingTheGridForm
+      .getByRole("radio", { name: "Option 1" })
+      .check({ force: true });
+    const radioStatus = await usingTheGridForm
+      .getByRole("radio", { name: "Option 1" })
+      .isChecked();
+    expect(radioStatus).toBeTruthy();
+    await expect(
+      usingTheGridForm.getByRole("radio", { name: "Option 1" })
+    ).toBeChecked();
+
+    await usingTheGridForm
+      .getByRole("radio", { name: "Option 2" })
+      .check({ force: true });
+    expect(
+      usingTheGridForm.getByRole("radio", { name: "Option 1" }).isChecked()
+    ).toBeFalsy();
+    expect(
+      usingTheGridForm.getByRole("radio", { name: "Option 2" }).isChecked()
+    ).toBeTruthy();
+  });
+});
