f328625db2e26890aad78d3f248b21bc6391b717
Marek Szumny <lukmarcus@gmail.com>
2025-02-07 13:55:43 +0100

    lists and dropdowns test added

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -86,3 +86,29 @@ test("Checkboxes", async ({ page }) => {
     expect(await box.isChecked()).toBeTruthy();
   }
 });
+
+test("Lists & dropdowns", async ({ page }) => {
+  const dropDownMenu = page.locator("ngx-header nb-select");
+  await dropDownMenu.click();
+
+  // const optionList = page.getByRole('list').locator('nb-option')
+  const optionList = page.locator("nb-option-list nb-option");
+  await expect(optionList).toHaveText(["Light", "Dark", "Cosmic", "Corporate"]);
+  await optionList.filter({ hasText: "Cosmic" }).click();
+
+  const header = page.locator("nb-layout-header");
+  await expect(header).toHaveCSS("background-color", "rgb(50, 50, 89");
+
+  const colors = {
+    Light: "rgb(255, 255, 255)",
+    Dark: "rgb(34, 43, 69)",
+    Cosmic: "rgb(50, 50, 89)",
+    Corporate: "rgb(255, 255, 255)",
+  };
+
+  for (const color in colors) {
+    await dropDownMenu.click();
+    await optionList.filter({ hasText: color }).click();
+    await expect(header).toHaveCSS("background-color", colors[color]);
+  }
+});
