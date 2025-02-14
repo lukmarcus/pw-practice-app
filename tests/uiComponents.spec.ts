29e4816dabee0ab65ff0bfd8568e60dd6bac0ce6
Marek Szumny <lukmarcus@gmail.com>
2025-02-14 23:06:39 +0100

    sliders test added

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -225,3 +225,30 @@ test("Datepicker", async ({ page }) => {
     .click();
   await expect(calendarInputField).toHaveValue(dateToAssert);
 });
+
+test("Sliders", async ({ page }) => {
+  const tempGauge = page.locator(
+    '[tabtitle="Temperature"] ngx-temperature-dragger circle'
+  );
+  await tempGauge.evaluate((node) => {
+    node.setAttribute("cx", "232.630");
+    node.setAttribute("cy", "232.630");
+  });
+  await tempGauge.click();
+
+  const tempBox = page.locator(
+    '[tabtitle="Temperature"] ngx-temperature-dragger'
+  );
+  await tempBox.scrollIntoViewIfNeeded();
+
+  const box = await tempBox.boundingBox();
+  const x = box.x + box.width / 2;
+  const y = box.y + box.height / 2;
+  await page.mouse.move(x, y);
+  await page.mouse.down();
+  await page.mouse.move(x + 100, y);
+  await page.mouse.move(x + 100, y + 100);
+  await page.mouse.up();
+
+  await expect(tempBox).toContainText("30");
+});
