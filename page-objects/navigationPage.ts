fa6cfe8fd5dd433ba5d2804124f5989f8ef30336
Marek Szumny <lukmarcus@gmail.com>
2025-04-01 23:03:16 +0200

    locators separated from navigation page methods

--- page-objects/navigationPage.ts
+++ page-objects/navigationPage.ts
@@ -1,35 +1,45 @@
-import { Page } from "@playwright/test";
+import { Page, Locator } from "@playwright/test";
 
 export class NavigationPage {
   readonly page: Page;
+  readonly formLayoutsMenuItem: Locator;
+  readonly datePickerMenuItem: Locator;
+  readonly smartTableMenuItem: Locator;
+  readonly toastMenuItem: Locator;
+  readonly tooltipMenuItem: Locator;
 
   constructor(page: Page) {
     this.page = page;
+    this.formLayoutsMenuItem = this.page.getByText("Form Layouts");
+    this.datePickerMenuItem = this.page.getByText("Datepicker");
+    this.smartTableMenuItem = this.page.getByText("Smart Table");
+    this.toastMenuItem = this.page.getByText("Toastr");
+    this.tooltipMenuItem = this.page.getByText("Tooltip");
   }
 
   async formLayoutsPage() {
     await this.selectGroupMenuItem("Forms");
-    await this.page.getByText("Form Layouts").click();
+    await this.formLayoutsMenuItem.click();
   }
 
   async datePickerPage() {
     await this.selectGroupMenuItem("Forms");
-    await this.page.getByText("Datepicker").click();
+    await this.datePickerMenuItem.click();
   }
 
   async smartTablePage() {
     await this.selectGroupMenuItem("Tables & Data");
-    await this.page.getByText("Smart Table").click();
+    await this.smartTableMenuItem.click();
   }
 
   async toastPage() {
     await this.selectGroupMenuItem("Modal & Overlays");
-    await this.page.getByText("Toastr").click();
+    await this.toastMenuItem.click();
   }
 
   async tooltipPage() {
     await this.selectGroupMenuItem("Modal & Overlays");
-    await this.page.getByText("Tooltip").click();
+    await this.tooltipMenuItem.click();
   }
 
   private async selectGroupMenuItem(groupItemTitle: string) {
