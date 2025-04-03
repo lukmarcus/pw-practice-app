f81f923480b9fa9f13acbf4e84acac59533a12e2
Marek Szumny <lukmarcus@gmail.com>
2025-04-03 19:18:19 +0200

    new datepicker page with parametrized methods

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,6 +1,7 @@
 import { test } from "@playwright/test";
 import { NavigationPage } from "../page-objects/navigationPage";
 import { FormLayoutsPage } from "../page-objects/formLayoutsPage";
+import { DatepickerPage } from "../page-objects/datepickerPage";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:4200/");
@@ -18,6 +19,7 @@ test("Navigate to form page", async ({ page }) => {
 test("Parametrized methods", async ({ page }) => {
   const navigateTo = new NavigationPage(page);
   const onFormLayoutsPage = new FormLayoutsPage(page);
+  const onDatepickerPage = new DatepickerPage(page);
 
   await navigateTo.formLayoutsPage();
   await onFormLayoutsPage.submitUsingTheGridFormWithCredentialsAndSelectOption(
@@ -30,4 +32,7 @@ test("Parametrized methods", async ({ page }) => {
     "john@test.com",
     true
   );
+  await navigateTo.datePickerPage();
+  await onDatepickerPage.selectCommonDatePickerDateFromToday(5);
+  await onDatepickerPage.selectDatepickerWithRangeFromToday(6, 15);
 });
